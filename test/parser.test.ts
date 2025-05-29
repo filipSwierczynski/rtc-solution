c3ca77a566cf36bebaf7319725be51990aaaf540
Filip Swierczynski <swirek.gw@hotmail.com>
2025-05-29 20:50:41 +0200

    added parser test

---
+++ test/parser.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { parseLine } from "../src/parser";
+
+const mockObj = {sport: 'FOOTBALL', competition: 'UEFA', home: 'Juventus', away: 'PSG', status: 'PRE'}
+
+describe('parseLine', () => {
+    const line = 'evtId,sport,competition,1748500000000,home,away,status';
+    expect(parseLine(line, mockObj)).toEqual({
+        id: 'evtId',
+        status: 'PRE',
+        startTime: new Date(1748500000000).toISOString(),
+        sport: 'FOOTBALL',
+        competition: 'UEFA',
+        scores: {
+
+        },
+        competitors: {
+            HOME: { type: 'HOME', name: 'Juventus'},
+            AWAY: { type: 'AWAY', name: 'PSG'}
+        }
+    })
+})
