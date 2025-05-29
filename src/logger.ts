ade4df05f460839ab2e70fb60e48853a65c7240f
Filip Swierczynski <swirek.gw@hotmail.com>
2025-05-29 22:01:11 +0200

    removed pino, added separate loggers

--- src/logger.ts
+++ src/logger.ts
@@ -1,1 +1,15 @@
-import pino from 'pino';
+
+export const logScoreChange = (id: string, newScores: unknown) => {
+console.log(JSON.stringify({
+    type: 'score-changed',
+    id,
+    scores: newScores
+}))
+}
+
+export const logStatusChanges = (id: string, from: string, to: string) => {
+    console.log(JSON.stringify({
+        type: 'status-changed',
+        id, from, to
+    }))
+}
