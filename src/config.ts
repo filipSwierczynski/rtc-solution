697148ab64f7008b3525f155f1c300eb7f7645d5
Filip Swierczynski <swirek.gw@hotmail.com>
2025-05-29 22:32:28 +0200

    updated host to 3000 to match docker simulation,

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SIM_URL = process.env.SIM_URL || 'http://localhost:4000';
+export const SIM_URL = process.env.SIM_URL || 'http://localhost:3000';
 export const PORT = Number(process.env.PORT) || 3000;
