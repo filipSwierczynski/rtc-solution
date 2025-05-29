19ee8c0c40a7b024a09a1d3738f194dc0be480d7
Filip Swierczynski <swirek.gw@hotmail.com>
2025-05-29 20:34:17 +0200

    added mapper function

--- src/mapper.ts
+++ src/mapper.ts
@@ -1,3 +1,12 @@
 export function parseMappings(blob: string): Record<string, string> {
+    const map: Record<string, string> = {};
     
+    if(!blob) return map;
+    blob.split(';').forEach(pair => {
+        if (!pair) return;
+        const idx = pair.indexOf(':');
+        if (idx === -1) return;
+        map[pair.slice(0, idx)] = pair.slice(idx + 1);
+    });
+    return map;
 }
