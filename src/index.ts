aba3352bd925e1f06115a5e8b2438a10fb162855
Filip Swierczynski <swirek.gw@hotmail.com>
2025-05-29 22:17:47 +0200

    bootstraped the server creation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import { StateStore } from './store';
+import { startPoller } from './poller';
+import { createServer } from './server';
+import { PORT } from './config';
+
+const store  = new StateStore();
+startPoller(store);
+
+createServer(store).listen(PORT, () => console.log(`API listening on :${PORT}`));
